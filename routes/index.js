6c4952720b0812061e2204fa97a7b7bd8012b7fd
Nabil <nhamri35@gmail.com>
2024-01-25 11:52:53 +0100

    feat(main): continue

--- routes/index.js
+++ routes/index.js
@@ -31,12 +31,43 @@ function getCoordinates(address) {
 }
 
 function getTrack(adressA, adressB){
+const nominatimUrl = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(adressA)}`;
+    const nominatimUrl2 = `https://nominatim.openstreetmap.org/search?format=json&q=${encodeURIComponent(adressB)}`;
+    axios.get(nominatimUrl)
+        .then(response => {
+            // Vérifier si la réponse est valide
+            if (response.data && response.data.length > 0) {
+                // Récupérer les coordonnées GPS
+                const latitude = response.data[0].lat;
+                const longitude = response.data[0].lon;
+
+                console.log(`Coordonnées GPS de ${adressA}: Latitude ${latitude}, Longitude ${longitude}`);
+            } else {
+                console.error('Aucune coordonnée GPS trouvée pour cette adresse.');
+            }
+        })
+        .catch(error => console.error('Erreur de réseau :', error));
+    axios.get(nominatimUrl2)
+        .then(response => {
+            // Vérifier si la réponse est valide
+            if (response.data && response.data.length > 0) {
+                // Récupérer les coordonnées GPS
+                const latitude = response.data[0].lat;
+                const longitude = response.data[0].lon;
+
+                console.log(`Coordonnées GPS de ${adressB}: Latitude ${latitude}, Longitude ${longitude}`);
+            } else {
+                console.error('Aucune coordonnée GPS trouvée pour cette adresse.');
+            }
+        })
+        .catch(error => console.error('Erreur de réseau :', error));
 
 }
 
 // Utilisation de la fonction avec une adresse spécifique
 //const adresse = '1600 Amphitheatre Parkway, Mountain View, CA';
 //getCoordinates(adresse);
+getTrack('202C rue victor rimmel', '13 rue du grand rabbin haguenauer');
 
 
 
